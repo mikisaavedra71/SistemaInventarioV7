8d55358424787b72cb531fdbe31793c446881be4
Miguel Saavedra <miguelsaavedrame@gmail.com>
2023-07-26 07:39:21 -0400

    Registro de Usuarios

--- SistemaInventario.Utilidades/DS.cs
+++ SistemaInventario.Utilidades/DS.cs
@@ -5,5 +5,11 @@
         public const string Exitosa = "Exitosa";
         public const string Error = "Error";
         public const string ImagenRuta = @"\imagenes\producto\";
+
+        public const string Role_Admin = "Admin";
+        public const string Role_Cliente = "Cliente";
+        public const string Role_Inventario = "Inventario";
+
+
     }
 }
